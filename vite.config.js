28ca7fb28464e26c0a84f52c766dbe49a7e0b731
orewagaurav <gauravkumar47967@gmail.com>
2025-06-28 14:52:15 +0530

    Password Generator

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Password_Generator/",
+    // base: "/Password_Generator/",
+    
+  base: isGitHubPages ? '/Password_Generator/' : '/',
   plugins: [react(),tailwindcss()],
 })
